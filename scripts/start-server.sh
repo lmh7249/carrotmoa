7114f854dcd4f67ffc8b790c8e3cef7ee051d58e
SungKyu Lim <lmh7249@naver.com>
2024-12-12 17:02:35 +0900

    test

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,6 +4,9 @@ echo "--------------- 서버 배포 시작 -----------------"
 cd /home/ubuntu/carrotmoa
 sudo fuser -k -n tcp 8080 || true
 # Nginx 재시작
+
+# 환경변수 직접 export
+export JASYPT_PASSWORD="${JASYPT_PASSWORD}"
 nohup java -Djasypt.encryptor.password=$JASYPT_PASSWORD -jar project.jar > ./output.log 2>&1 &
 
 sleep 10
